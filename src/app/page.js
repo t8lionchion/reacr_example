5f088fe7492093a4bbf9f3b8490bb7be855dd4bb
tygfuidseujzx@gmail.com <tygfuidseujzx@gmail.com>
2025-07-15 10:59:41 +0800

    建立react的第一個專案

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>你好嗎</h1>
       <main className={styles.main}>
         <Image
           className={styles.logo}
