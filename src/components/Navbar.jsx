9698cbe9050f4a8b9c2c6b3e0d2d220c1ce83623
tygfuidseujzx@gmail.com <tygfuidseujzx@gmail.com>
2025-07-17 11:27:27 +0800

    fix(Navbar): 將Navbar中的<a>標籤替換為<Link>組件

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
+import Link from "next/link";
 export function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+                <Link className="navbar-brand" href="index.html">Start Bootstrap</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ms-auto py-4 py-lg-0">
-                        <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-                        <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-                        <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-                        <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+                        <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/about">About</Link></li>
+                        <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</Link></li>
+                        <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</Link></li>
                     </ul>
                 </div>
             </div>
